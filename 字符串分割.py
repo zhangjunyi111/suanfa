720fba50a299121b80dc7408f9db613284d192ec
zhangjunyi111 <18435205109@163.com>
2024-12-07 13:54:54 +0800

    2024年12月7日

--- 字符串分割.py
+++ 字符串分割.py
@@ -5,8 +5,19 @@
 # 改变心态，每个程序和功能都认真对待，不可以轻视和懈怠
 # 改变心态，每个程序和功能都认真对待，不可以轻视和懈怠
 # 改变心态，每个程序和功能都认真对待，不可以轻视和懈怠
+# isupper函数判断字符串是否由大写字母组成
+# islower函数
+# upper函数将字符串转化为大写
+# lower函数将字符串转化为小写
+
+
 
 def count(s):
+    '''
+    统计一个对象中的数量，可以考虑用字典
+    :param s:
+    :return:
+    '''
     char_dict = {}
     char_dict["upper"] = 0
     char_dict["lower"] = 0
@@ -36,6 +47,7 @@ def main():
     s_new = ''.join(s[1:])
     res = []
     for i in range(0, len(s_new), k):
+        # 在区间内按区间遍历切割，区间为k的时候，判断 i+k的长度是否大于len(s)的长度就行。
         if i + k > len(s_new):
             children_str = s_new[i:]
             children_str = convert(children_str)
