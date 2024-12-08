5ce7329f2641aa479e7e340c7c4a194cb413c738
zhangjunyi111 <18435205109@163.com>
2024-12-08 11:58:12 +0800

    2024年12月8日 工号不够用了怎么办 增加max函数，字母的最小值为1

--- 工号不够用了怎么办.py
+++ 工号不够用了怎么办.py
@@ -3,7 +3,7 @@ import math
 
 def main():
     num_people, num_letter = map(int, input().split())
-    num_number = math.ceil(math.log10(num_people / pow(26, num_letter)
+    num_number = max(1,math.ceil(math.log10(num_people / pow(26, num_letter))))
     print(num_number)
 
 
