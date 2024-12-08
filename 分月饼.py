d108f66125387234c0ee7f6fa926bf0d007d944a
zhangjunyi111 <18435205109@163.com>
2024-12-08 15:06:58 +0800

    2024年12月8日 分月饼 新传递一个参数n,当达到递归深度时，method添加n-sum(method)

--- 分月饼.py
+++ 分月饼.py
@@ -1,5 +1,3 @@
-
-
 # 考察递归
 
 # 分月饼
@@ -7,28 +5,32 @@
 # 思路
 # 按照月饼的数量进行排序，第n-1个比第n个最多多3个
 distribute_method = []
-def distribute(num,have_distributed,method,m):
-    global  distribute_method
+
+
+def distribute(num, have_distributed, method, m, n):
+    global distribute_method
     if have_distributed == m:
         distribute_method.append(method)
         return
 
-    for i in range(1,num):
-        method.append(i)
+    for i in range(1, num):
         have_distributed += 1
-        distribute(num - i, have_distributed, method,m)
+        if have_distributed == m:
+            method.append(n - sum(method))
+        distribute(num - i, have_distributed, method, m, n)
         have_distributed -= 1
         method.pop()
 
+
 def main():
-    m,n = map(int,input().split())
+    m, n = map(int, input().split())
     have_distributed = 0
-    method =[]
+    method = []
     # 列出所有的分配方案
     # 尝试从n个月饼中分配1-n个
-    distribute(n,have_distributed,method,m)
-
+    distribute(n, have_distributed, method, m, n)
 
     # 排除不符合要求的
+
 
 main()
