57c1bc1e1efca794b059ab348181f8fbd655baa3
zhangjunyi111 <18435205109@163.com>
2024-12-08 11:56:48 +0800

    2024年12月8日 工号不够用了怎么办

---
+++ 工号不够用了怎么办.py
@@ -0,0 +1,10 @@
+import math
+
+
+def main():
+    num_people, num_letter = map(int, input().split())
+    num_number = math.ceil(math.log10(num_people / pow(26, num_letter)
+    print(num_number)
+
+
+main()
