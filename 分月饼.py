e90bc5291db57913b18345ed8d0a728f39d82bc3
zhangjunyi111 <18435205109@163.com>
2024-12-08 15:15:33 +0800

    2024年12月8日 分月饼 对结果进行提前去重

--- 分月饼.py
+++ 分月饼.py
@@ -10,13 +10,17 @@ distribute_method = []
 def distribute(num, have_distributed, method, m, n):
     global distribute_method
     if have_distributed == m:
-        distribute_method.append(method)
+        # 提前去重，不添加到结果列表
+        if method.sort() in distribute_method:
+            return
+        distribute_method.append(method.copy())
         return
 
     for i in range(1, num):
         have_distributed += 1
         method.append(i)
         if have_distributed == m:
+            method.pop()
             method.append(n - sum(method))
 
         distribute(num - i, have_distributed, method, m, n)
@@ -31,7 +35,7 @@ def main():
     # 列出所有的分配方案
     # 尝试从n个月饼中分配1-n个
     distribute(n, have_distributed, method, m, n)
-
+    print(distribute_method)
     # 排除不符合要求的
 
 
