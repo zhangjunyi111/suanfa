3f05864a3072f51d33a31f69f21e4cd64acd95a0
zhangjunyi111 <18435205109@163.com>
2024-12-08 16:31:56 +0800

    2024年12月8日 考古学家的考古问题

---
+++ 考古学家的考古问题.py
@@ -0,0 +1,34 @@
+# start 2024年12月8日 16:17分开始
+# end
+def dfs(current_content, n, content, visited):
+    global res
+    if len(current_content) == n:
+        res.append(current_content.copy())
+        return
+
+    for i in range(n):
+        if not visited[i]:
+            current_content.append(content[i])
+            visited[i] = True
+            dfs(current_content, n, content,visited)
+            current_content.pop()
+            visited[i] = False
+
+
+res = []
+
+
+def main():
+    # 接收数量
+    n = int(input().strip())
+
+    # 接收内容
+    content = input().strip().split()
+
+    # 进行全新的排列组合
+    current_content = []
+    visited = [False] * n
+    dfs(current_content, n, content, visited)
+
+
+main()
