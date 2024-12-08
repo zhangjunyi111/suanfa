35c9ccb00781fd108caad06e35afa6b1f880580f
zhangjunyi111 <18435205109@163.com>
2024-12-08 18:02:34 +0800

    2024年12月8日 最小矩阵宽度  重复生成新的numbers_dict。

--- 最小矩阵宽度.py
+++ 最小矩阵宽度.py
@@ -8,7 +8,7 @@
 
 def contain_all_number(i, j, matrix, numbers_dict):
     for row in range(len(matrix)):
-        for index in range(i, j):
+        for index in range(i, j + 1):
             if matrix[row][index] in numbers_dict:
                 numbers_dict[matrix[row][index]] -= 1
                 if numbers_dict[matrix[row][index]] == 0:
@@ -27,29 +27,35 @@ def main():
     # 结收数字的数量
     count = int(input())
 
+
+
     # 接收数字
     numbers = list(map(int, input().split()))
-    # 将数字存储到字典
-    numbers_dict = {}
-    for number in numbers:
-        if number not in numbers_dict:
-            numbers_dict[number] = 0
-        numbers_dict[number] += 1
+
+    def get_number_dict(numbers):
+        # 将数字存储到字典
+        numbers_dict = {}
+        for number in numbers:
+            if number not in numbers_dict:
+                numbers_dict[number] = 0
+            numbers_dict[number] += 1
+        return numbers_dict
 
     # 初始化最小矩阵宽度
     min_width = float('inf')
-
+    min_left = -1
+    min_right = -1
     # 逐步缩小矩阵的宽度，记录过程中min_width
-
-    left = 0
-    right = m
 
     for left in range(m):
         for right in range(left, m):
-            if contain_all_number(left, right+1, matrix, numbers_dict):
+            numbers_dict = get_number_dict(numbers)
+            if contain_all_number(left, right, matrix, numbers_dict):
                 min_width = min(min_width, right - left + 1)
+                min_left = left
+                min_right = right
 
-    print(min_width)
+    print(min_width, min_left, min_right)
 
 
 main()
