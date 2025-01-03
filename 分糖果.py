d68e646bdda37ae33f2a018eb3feb58e4257acae
zhangjunyi111 <18435205109@163.com>
2025-01-03 19:24:20 +0800

    2025年1月1日  分糖果

--- 分糖果.py
+++ 分糖果.py
@@ -7,11 +7,19 @@ def main():
     x = int(input())
     count = 0
     while x != 1:
+        if x ==3:
+            count +=2
+            break
         if x % 2 != 0 :
             if (x+1) //2 %2 == 0:
-                x = x//4
-                count += 1
-
+                x += 1
             else:
-                x = x %2
-                count += 1
+                x -= 1
+            #     拿出一颗糖果或者放回一颗糖果
+            count += 1
+        # 模拟分糖果的操作
+        x //= 2
+        count +=1
+    print(count)
+
+main()
