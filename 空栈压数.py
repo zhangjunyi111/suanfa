8be563056a3fe796e09625a7acd847c329f953b9
zhangjunyi111 <18435205109@163.com>
2025-01-04 20:19:46 +0800

    2025年1月4日 打印任务排序修改append的逻辑、修改index减小的位置

--- 空栈压数.py
+++ 空栈压数.py
@@ -4,25 +4,22 @@ stack = []
 input_sequence = list(map(int, input().split()))
 
 stack.append(input_sequence[0])
-index = len(stack) - 1
+
 
 for current_number in input_sequence[1:]:
+    index = len(stack) - 1
     _sum = current_number
     while index >= 0 and _sum > 0:
         _sum -= stack[index]
-        index -= 1
+
         if _sum == 0:
             # 模拟出栈操作
             stack = stack[: index]
+            # 更新current_number的值
+            current_number = current_number * 2
+            break
+        index -= 1
 
-            # 模拟双倍进栈操作
-            new_number = current_number * 2
-            stack.append(new_number)
-
-    if _sum < 0:
-        stack.append(current_number)
-
-    if len(stack) ==0:
-        stack.append(current_number)
+    stack.append(current_number)
 
 print(stack)
