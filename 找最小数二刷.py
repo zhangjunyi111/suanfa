f094bdc95dfefe4c4c309efa36bb84d502affd9d
zhangjunyi111 <18435205109@163.com>
2025-01-04 12:18:42 +0800

    2025年1月1日  找最小数 增加对0的判断

--- 找最小数二刷.py
+++ 找最小数二刷.py
@@ -1,30 +1,31 @@
-
-
 # 找最小数,移除N位数后，剩下的数最小
 # 9876543
 
 def main():
-    num = input()
+    num = input().strip()
     N = int(input())
 
     # 应该维护一个单调递减的栈
     stack = []
-
-
     # 将num转化一下，转化为一个数字列表
-    num = list(map(int,num))
+    num = list(map(int, num))
 
     stack.append(num[0])
-    for i in range(1,len(num)):
-        while N and stack and   stack[-1] >num[i]:
+    for i in range(1, len(num)):
+        while N and stack and stack[-1] > num[i]:
             stack.pop()
-            N-=1
+            N -= 1
         stack.append(num[i])
 
-    while N :
+    while N:
         stack.pop()
         N -= 1
 
-    print(''.join(list(map(str,stack))).lstrip('0'))
+    res = ''.join(list(map(str, stack))).lstrip('0')
+    if res:
+        print(res)
+    else:
+        print(0)
+
 
 main()
