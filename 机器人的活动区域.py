eb9fb0576c2441251ca7dde99d0be2cd2fea7159
zhangjunyi111 <18435205109@163.com>
2025-01-05 13:46:42 +0800

    2025年1月4日 机器人的活动范围

---
+++ 机器人的活动区域.py
@@ -0,0 +1,38 @@
+# 定义好深度优先遍历的函数
+import sys
+
+
+def dfs(matrix, i, j):
+    range = 1
+    # 初始化一个方向列表
+    directions = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+    pass
+    for dir in directions:
+        x = i + dir[0]
+        y = i + dir[1]
+        if 0 <= x < m and 0 <= y < n and matrix[m][n] <= 1:
+            range += dfs(matrix, x, y)
+
+    return range
+
+
+# 接收区域的长度和宽度，并接收每个网格上的数字
+m = 0
+n = 0
+matrix = []
+max_range = 0
+for line in sys.stdin:
+    if not m and not n:
+        m, n = map(int, line.split())
+
+    else:
+        matrix.append(map(int, line.split()))
+        if len(matrix) == m:
+            break
+
+# 遍历网格的每一个位置，进行深度优先遍历，寻找最大的活动范围
+
+for i in range(m):
+    for j in range(n):
+        activite_range = dfs(matrix, i, j)
+        max_range = max(activite_range, max_range)
