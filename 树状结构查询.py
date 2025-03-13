d58144bde832b7dcf4aff65a65a69b1fdc90de34
zhangjunyi111 <18435205109@163.com>
2025-03-13 08:13:00 +0800

    3月13日 栈代码完善

--- 树状结构查询.py
+++ 树状结构查询.py
@@ -7,14 +7,14 @@
 # e c
 # f d
 # c
+import sys
 
 
 def main():
     # 接收父子关系的行数
     relation_N = int(input())
 
-    # 接收要查询的node
-    query_node = input()
+
 
     # 将树的关系存储到字典中，初始化一个存储树的字典
 
@@ -28,16 +28,28 @@ def main():
             tree_dict[parent_node] = set()
         tree_dict[parent_node].add(child_node)
 
+    # 接收要查询的node
+    query_node = input()
     # 记得删除打印语句
-    print(tree_dict)
+    # print(tree_dict)
 
     # 初始化结果列表，存储结果集合
     result = []
 
+    if query_node not in tree_dict:
+        print('查询的节点不在树中')
+        sys.exit()
+
     # 初始化队列
-    queue = []
-    queue.append(tree_dict[query_node])
+    queue = list(tree_dict[query_node])
     while queue:
         child_node = queue.pop(0)
+        result.append(child_node)
         if child_node in tree_dict:
-            tree_dict[child_node]
+            queue.extend(list(tree_dict[child_node]))
+
+    for node in sorted(result):
+        print(node)
+
+
+main()
